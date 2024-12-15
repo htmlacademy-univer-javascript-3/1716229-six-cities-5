1b9f466e6a741c31a69c4351040b28ceb01213d4
oNIKOLAYSVo <nikolaysavchenkoprog@gmail.com>
2024-12-15 19:16:04 +0300

    create ProtectedRote

---
+++ src/pages/ProtectedRote.tsx
@@ -0,0 +1,22 @@
+import { Navigate } from "react-router-dom";
+import { AppRoute, AuthorizationStatus } from "../const";
+
+type TProtectedRouteProps = {
+    restrictedFor: AuthorizationStatus;
+    redirectTo: AppRoute;
+    children: JSX.Element;
+}
+
+export default function ProtectedRote({
+    restrictedFor,
+    redirectTo,
+    children
+}: TProtectedRouteProps) {
+    const authorizationStatus = AuthorizationStatus.NoAuth;
+
+    return restrictedFor === authorizationStatus ? (
+        <Navigate to={redirectTo} />
+    ) : (children);
+
+
+}
